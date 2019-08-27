3b84acda243e4fe229a2b1ebf6e267a14fb0fe15
shiyou <8533334@qq.com>
2019-08-27 18:02:03 +0800

    add some case

--- src/app.js
+++ src/app.js
@@ -5,8 +5,12 @@ const fs = require("fs");
 const promisify = require("util").promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
+const Handlebars = require("handlebars");
+const tplPath = path.join(__dirname, "./template/dir.tpl");
+const source = fs.readFileSync(tplPath);
+const template = Handlebars.compile(source.toString());
 
-const exportStats = async function(filePath, res) {
+const exportStats = async function(filePath, res, req) {
   try {
     const stats = await stat(filePath);
     if (stats.isFile()) {
@@ -17,9 +21,15 @@ const exportStats = async function(filePath, res) {
     } else if (stats.isDirectory()) {
       // 路径为文件夹，我们输出文件列表
       const files = await readdir(filePath);
+      // 要传入到模板中的数据
+      const data = {
+        title: path.basename(filePath),
+        dir: req.url,
+        files
+      };
       res.statusCode = 200;
-      res.setHeader("Content-Type", "text/plain");
-      res.end(files.join(","));
+      res.setHeader("Content-Type", "text/html");
+      res.end(template(data));
     }
   } catch (e) {
     res.statusCode = 404;
@@ -29,9 +39,8 @@ const exportStats = async function(filePath, res) {
 };
 
 const server = http.createServer((req, res) => {
-  const url = req.url;
-  const filePath = path.join(conf.root, url); // 当前目录的绝对路径 + 访问的url路径
-  exportStats(filePath, res);
+  const filePath = path.join(conf.root, req.url); // 当前目录的绝对路径 + 访问的url路径
+  exportStats(filePath, res, req);
 });
 
 server.listen(conf.port, conf.hostname, () => {
--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
   hostname: '127.0.0.1',
-  port: 3000,
+  port: 3003,
   root: process.cwd()
 };
