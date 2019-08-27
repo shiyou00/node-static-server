9824cebcf97f5ca30b15682aa26605cc24602114
shiyou <8533334@qq.com>
2019-08-27 10:59:24 +0800

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": ["eslint:recommended","prettier"],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+      "no-console":["error",{
+        "allow": ["warn", "error", "info"]
+      }]
+    }
+};
